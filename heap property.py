5481d189b0449b5b2446d7b2bf5ca516e53c23eb
meghanaravuri8-eng <meghanaravuri8@gmail.com>
2026-08-17 21:46:03 +0530

    Add files via upload

---
+++ heap property.py
@@ -0,0 +1,7 @@
+import heapq
+priority_queue=[]
+heapq.heappush(priority_queue,2)
+heapq.heappush(priority_queue,3)
+heapq.heappush(priority_queue,1)
+heapq.heappush(priority_queue,0)
+print(priority_queue)
