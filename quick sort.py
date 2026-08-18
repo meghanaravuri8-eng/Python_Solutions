f167732a96e5452a0e81213b76b97a3bf01947ff
meghanaravuri8-eng <meghanaravuri8@gmail.com>
2026-08-18 19:12:21 +0530

    Add files via upload

---
+++ quick sort.py
@@ -0,0 +1,17 @@
+def partition(arr,low,high):
+    pivot=arr[high]
+    i=low-1
+    for j in range(low,high):
+        if arr[j]<pivot:
+            i+=1
+            arr[i],arr[j]=arr[j],arr[i]
+    arr[i+1],arr[high]=arr[high],arr[i+1]
+    return i+1
+def quick(arr,low,high):
+    if low<high:
+        k=partition(arr,low,high)
+        quick(arr,low,k-1)
+        quick(arr,k+1,high)
+arr=[3,2,1]
+quick(arr,0,len(arr)-1)
+print(arr)
